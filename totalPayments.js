d6ffee190587e26a78544ed4ae46a235f0132b14
tigranqaramyan <tigranqaramyan606@gmail.com>
2024-09-01 18:58:36 +0400

    totalSum

---
+++ totalPayments.js
@@ -0,0 +1,17 @@
+const payments = [
+    { studentId: 1, studentName: "John Doe", payedAmount: 1600, year: 2020 },
+    { studentId: 1, studentName: "John Doe", payedAmount: 1500, year: 2021 },
+    { studentId: 1, studentName: "John Doe", payedAmount: 1400, year: 2022 },
+    { studentId: 1, studentName: "John Doe", payedAmount: 1300, year: 2023 },
+    { studentId: 2, studentName: "Lu Kang", payedAmount: 1500, year: 2020 },
+    { studentId: 2, studentName: "Lu Kang", payedAmount: 1400, year: 2021 },
+    { studentId: 2, studentName: "Lu Kang", payedAmount: 1300, year: 2023 },
+    { studentId: 2, studentName: "Lu Kang", payedAmount: 1200, year: 2023 }
+];
+
+
+const totalSum = payments.reduce((total, payment) => {
+    return total + payment.payedAmount;
+}, 0);
+
+console.log(totalSum)
